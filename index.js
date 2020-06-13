98da0745dd31444d0b698c986a454df9e0ac9b72
Rafinha <arruda.rafa.ra@gmail.com>
2020-06-13 14:22:17 -0400

    atualizando o código para print --> ('imagem')

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setViewport({
+      width: 600,
+      height: 690
+    });
+
     // Navega até a pagina destino
     await page.goto('https://www.uffs.edu.br/campi/chapeco/restaurante_universitario');
-    await page.waitFor('#content-core > div > span > dl:nth-child(19) > dd');
-    // Tira um Screenshot da página
-    await page.screenshot({path: 'example.png'});
-    await browser.close();
-})();
+    
+    
+    await page.evaluate(() => {
+      document.querySelector("#content-core > div > span > p:nth-child(12)").scrollIntoView();
 
+     
+    });
+
+    // Tira um Screenshot da página
+    await page.screenshot({ path: 'example.png', });
+    await browser.close();
+
+
+})();
